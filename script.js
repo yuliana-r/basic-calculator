dc55ca0a1c73ddf223c04d151a8388631fc8f467
yuli-r <iuliana.rotaru234@gmail.com>
2022-01-30 22:53:27 +0000

    Amend event listeners for numbers and operators

--- script.js
+++ script.js
@@ -1,13 +1,147 @@
+let CURRENT_NUMBER;
+let CURRENT_OPERATOR;
+let OPERAND;
+let RESULT;
+let operatorIsPressed = false;
+
 const display = document.getElementById('display');
 const clearBtn = document.getElementById('clear');
 const numberBtn = document.querySelectorAll('.number');
+const operatorBtn = document.querySelectorAll('.operator');
 
-clearBtn.addEventListener('click', () => display.textContent = '');
+clearBtn.addEventListener('click', () => {
+    clearDisplay();
+});
 
+const equals = document.getElementById('equals');
+equals.addEventListener('click', () => {
+
+    if (CURRENT_NUMBER !== undefined && OPERAND === undefined && CURRENT_OPERATOR !== undefined) {
+
+        OPERAND = +display.textContent;
+        display.textContent = '';
+
+        // RESULT = operate(+CURRENT_NUMBER, +OPERAND, CURRENT_OPERATOR);
+        // CURRENT_NUMBER = +RESULT;
+        // display.textContent = CURRENT_NUMBER;
+
+        updateResult();
+        
+        CURRENT_OPERATOR = undefined;
+        OPERAND = undefined;
+
+        console.log(`equals`);
+        console.log(`current number: ${CURRENT_NUMBER}`);
+        console.log(`current operator: ${CURRENT_OPERATOR}`);
+        console.log(`current operand: ${OPERAND}`);
+    }
+});
 
 numberBtn.forEach(button => {
     button.addEventListener('click', () => {
-        display.textContent = (display.textContent + button.getAttribute('data-value')).substring(0,11);
+        if (display.textContent == CURRENT_NUMBER || display.textContent == OPERAND) {
+            display.textContent = '';
+            display.textContent = (display.textContent + button.getAttribute('data-value')).substring(0, 11);
+        } else {
+            display.textContent = (display.textContent + button.getAttribute('data-value')).substring(0, 11);
+        }
+    })
+})
+
+// operatorBtn.forEach(button => {
+//     button.addEventListener('click', () => {
+
+//         if (OPERAND !== undefined && CURRENT_OPERATOR !== undefined) {
+
+//             RESULT = operate(+CURRENT_NUMBER, +OPERAND, CURRENT_OPERATOR);
+//             CURRENT_NUMBER = +RESULT;
+//             display.textContent = CURRENT_NUMBER;
+//             OPERAND = undefined;
+//             CURRENT_OPERATOR = undefined;
+
+
+//         } else if (CURRENT_NUMBER !== undefined && OPERAND === undefined && CURRENT_OPERATOR !== undefined) {
+//             OPERAND = +display.textContent;
+//             display.textContent = '';
+//             RESULT = operate(+CURRENT_NUMBER, +OPERAND, CURRENT_OPERATOR);
+//             CURRENT_NUMBER = +RESULT;
+//             display.textContent = CURRENT_NUMBER;
+//             CURRENT_OPERATOR = button.getAttribute('data-value');
+//             OPERAND = undefined;
+
+//         } else {
+
+//             CURRENT_NUMBER = +display.textContent;
+//             CURRENT_OPERATOR = button.getAttribute('data-value');
+//             display.textContent = '';
+//         }
+//     })
+// })
+
+
+operatorBtn.forEach(button => {
+    button.addEventListener('click', () => {
+
+
+            // if (OPERAND !== undefined && CURRENT_OPERATOR !== undefined) {
+
+            //     // RESULT = operate(+CURRENT_NUMBER, +OPERAND, CURRENT_OPERATOR);
+            //     // CURRENT_NUMBER = +RESULT;
+            //     // display.textContent = CURRENT_NUMBER;
+
+            //     updateResult();
+
+            //     OPERAND = undefined;
+            //     CURRENT_OPERATOR = undefined;
+                
+
+
+            //     console.log(`if #1`);
+            //     console.log(`current number: ${CURRENT_NUMBER}`);
+            //     console.log(`current operator: ${CURRENT_OPERATOR}`);
+            //     console.log(`current operand: ${OPERAND}`);
+
+            // } else 
+            if (CURRENT_NUMBER !== undefined && OPERAND === undefined && CURRENT_OPERATOR !== undefined) {
+                OPERAND = +display.textContent;
+                display.textContent = '';
+
+                // RESULT = operate(+CURRENT_NUMBER, +OPERAND, CURRENT_OPERATOR);
+                // CURRENT_NUMBER = +RESULT;
+                // display.textContent = CURRENT_NUMBER;
+
+                updateResult();
+
+                CURRENT_OPERATOR = button.getAttribute('data-value');
+                OPERAND = undefined;
+
+                console.log(`if #2`);
+                console.log(`current number: ${CURRENT_NUMBER}`);
+                console.log(`current operator: ${CURRENT_OPERATOR}`);
+                console.log(`current operand: ${OPERAND}`);
+
+            } else if (CURRENT_NUMBER === undefined && CURRENT_OPERATOR === undefined) {
+                CURRENT_NUMBER = +display.textContent;
+                CURRENT_OPERATOR = button.getAttribute('data-value');
+                display.textContent = CURRENT_NUMBER;
+
+                console.log(`if #3`);
+                console.log(`current number: ${CURRENT_NUMBER}`);
+                console.log(`current operator: ${CURRENT_OPERATOR}`);
+                console.log(`current operand: ${OPERAND}`);
+
+            } else if (CURRENT_NUMBER !== undefined && CURRENT_OPERATOR === undefined && OPERAND === undefined) {
+                CURRENT_NUMBER = +display.textContent;
+                CURRENT_OPERATOR = button.getAttribute('data-value');
+                display.textContent = CURRENT_NUMBER;
+
+                console.log(`if #4`);
+                console.log(`current number: ${CURRENT_NUMBER}`);
+                console.log(`current operator: ${CURRENT_OPERATOR}`);
+                console.log(`current operand: ${OPERAND}`);
+
+            }
+        
     })
 })
 
@@ -38,4 +172,17 @@ function operate(a, b, operator) {
         case "/":
             return divide(a, b);
     }
+}
+
+function updateResult() {
+    RESULT = operate(+CURRENT_NUMBER, +OPERAND, CURRENT_OPERATOR);
+    CURRENT_NUMBER = +RESULT;
+    display.textContent = CURRENT_NUMBER;
+}
+
+function clearDisplay() {
+    display.textContent = '';
+    CURRENT_NUMBER = undefined;
+    CURRENT_OPERATOR = undefined;
+    OPERAND = undefined;
 }
